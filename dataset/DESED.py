b8c8353725558f69b237f18a656ceb7b023ba924
kbh <nine4409@sogang.ac.kr>
2022-03-29 21:31:43 +0900

    Data Generation

---
+++ dataset/DESED.py
@@ -0,0 +1,52 @@
+import sys,os
+sys.path.append("gpuRIR")
+
+from mixing import generate
+
+import glob
+import argparse
+
+import numpy as np
+import librosa
+import torch
+
+# utils
+from tqdm import tqdm
+from multiprocessing import Pool, cpu_count
+
+# Due to 'PySoundFile failed. Trying audioread instead' 
+import warnings
+warnings.filterwarnings('ignore')
+
+# param
+parser = argparse.ArgumentParser()
+parser.add_argument('--input_root', '-i', type=str, required=True)
+parser.add_argument('--output_root', '-o', type=str, required=True)
+parser.add_argument('--n_output', '-n', type=int, required=True)
+args = parser.parse_args()
+
+## ROOT
+root = args.input_root
+output_root = args.output_root
+n_output = args.n_output
+
+## PATH
+target_list = [x for x in glob.glob(os.path.join(root,'**','*.wav'),recursive=True)]
+print("Target Files : {}".format(len(target_list)))
+
+def process(idx):
+    # gen random parameters
+    n_source = np.random.randint(low=1,high=4)
+
+    list_sources = np.random.choice(target_list,n_source)
+
+    generate(output_root,list_sources,idx,50,shift=256,match="4sec")
+
+if __name__=='__main__': 
+    cpu_num = cpu_count()
+
+    os.makedirs(os.path.join(output_root),exist_ok=True)
+
+    arr = list(range(n_output))
+    with Pool(cpu_num) as p:
+        r = list(tqdm(p.imap(process, arr), total=len(arr),ascii=True,desc='processing'))
